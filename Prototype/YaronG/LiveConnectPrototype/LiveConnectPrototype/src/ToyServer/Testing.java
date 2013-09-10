ca77ef59c96aaadb015c8b064fc0e9eb5e2c60ca
yarong <yarong@microsoft.com>
2013-09-10 16:15:43 -0700

    Just putting in stubs for the live connect prototype

---
+++ Prototype/YaronG/LiveConnectPrototype/LiveConnectPrototype/src/ToyServer/Testing.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ToyServer;
+
+/**
+ *
+ * @author yarong
+ */
+public class Testing {
+    
+}
