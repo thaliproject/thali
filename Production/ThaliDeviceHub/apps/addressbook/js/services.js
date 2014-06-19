93cbe4d33dfc0001266e0cf6a5f27ec3182dc1a1
Ivan R. Judson <irjudson@gmail.com>
2014-06-19 07:27:01 -0700

    TDH synching complete.

--- Production/ThaliDeviceHub/apps/addressbook/js/services.js
+++ Production/ThaliDeviceHub/apps/addressbook/js/services.js
@@ -1,86 +1,100 @@
 angular.module('myApp.services', [])
     .factory('Contact', function($q, $rootScope) {
-        var db = new PouchDB('thali-contacts');
+        var dbname = 'thali-contacts';
+        var tdhdb = 'http://127.0.0.1:58000/'+dbname;
+        var db = new PouchDB(dbname);
+        // Create the tdh db if we need to
+        PouchDB.replicate(dbname, tdhdb, {live:true, create_target:true});
         return {
-        	create: function (contact) {
-        		var deferred = $q.defer();
-        		db.post(contact, function(err, response) {
-        			$rootScope.$apply(function() {
-        				if(response.ok) {
-        					deferred.resolve(response);
-        				} else {
-        					console.log("Failed to save new contact: " + err);
-        					deferred.reject(err);
-        				}
-        			});
-        		});
-        		return deferred.promise;
-        	},
-        	retrieve: function (contactId) {
-    	    	var deferred = $q.defer();
-    	        db.allDocs({include_docs:true}, function(err,response) {
-    	        	$rootScope.$apply(function() {
-    		 			if (response) {
-    		 				var map = Array.prototype.map;
-    		 				var returnValue = map.call(response.rows, function(x) { return x.doc;});
-    		 				if (contactId) {
-    		 					returnValue = returnValue.filter(function(contact) { return contact._id == contactId})[0];
-    		 				}
-    		 				deferred.resolve(returnValue);
-    		 			} else {
-    		 				console.log("Error getting all contacts: " + err);
-    		 				deferred.reject(err);
-    		 			}	        		
-    	        	});
-    	        });
-    	        return deferred.promise;
-        	}, 
-       		update: function (contact) {
-        		var deferred = $q.defer();
-        		db.post(contact, function(err, response) {
-        			$rootScope.$apply(function() {
-        				if(response.ok) {
-        					deferred.resolve(response);
-        				} else {
-        					console.log("Failed to update contact: " + err);
-        					deferred.reject(err);
-        				}
-        			});
-        		});
-        		return deferred.promise;
-        	},
-        	delete: function (contact) {
-        		console.log("In delete!");
-        		var deferred = $q.defer();
-        		db.remove(contact, function(err, response) {
-        			$rootScope.$apply(function() {
-        				if (response) {
-        					deferred.resolve(response);
-        				} else {
-        					console.log("Failed to delete contact: " + err);
-        					deferred.reject(err);
-        				}
-        			});
-        		});
-        		return deferred.promise;
-        	}
-    	}
+            create: function (contact) {
+                var deferred = $q.defer();
+                db.post(contact, function(err, response) {
+                    $rootScope.$apply(function() {
+                        if(response.ok) {
+                            db.replicate.to(tdhdb);
+                            deferred.resolve(response);
+                        } else {
+                            console.log("Failed to save new contact: " + err);
+                            deferred.reject(err);
+                        }
+                    });
+                });
+                return deferred.promise;
+            },
+            retrieve: function (contactId) {
+                var deferred = $q.defer();
+                db.allDocs({include_docs:true}, function(err,response) {
+                    $rootScope.$apply(function() {
+                        if (response) {
+                            var map = Array.prototype.map;
+                            var returnValue = map.call(response.rows, function(x) { return x.doc;});
+                            if (contactId) {
+                                returnValue = returnValue.filter(function(contact) { return contact._id == contactId})[0];
+                            }
+                            db.replicate.to(tdhdb);
+                            deferred.resolve(returnValue);
+                        } else {
+                            console.log("Error getting all contacts: " + err);
+                            deferred.reject(err);
+                        }                   
+                    });
+                });
+                return deferred.promise;
+            }, 
+            update: function (contact) {
+                var deferred = $q.defer();
+                db.post(contact, function(err, response) {
+                    $rootScope.$apply(function() {
+                        if(response.ok) {
+                            db.replicate.to(tdhdb);
+                            deferred.resolve(response);
+                        } else {
+                            console.log("Failed to update contact: " + err);
+                            deferred.reject(err);
+                        }
+                    });
+                });
+                return deferred.promise;
+            },
+            delete: function (contact) {
+                console.log("In delete!");
+                var deferred = $q.defer();
+                db.remove(contact, function(err, response) {
+                    $rootScope.$apply(function() {
+                        if (response) {
+                            db.replicate.to(tdhdb);
+                            deferred.resolve(response);
+                        } else {
+                            console.log("Failed to delete contact: " + err);
+                            deferred.reject(err);
+                        }
+                    });
+                });
+                return deferred.promise;
+            }
+        }
     })
     .run(function (){
-    	var db = new PouchDB('thali-contacts');
+        var dbname = 'thali-contacts';
+        var tdhdb = 'http://127.0.0.1:58000/'+dbname;
+        var db = new PouchDB(dbname);
 
-    	db.info().then(function(info) {
-    		// Initialize demo data
-    		if (info.doc_count < 1) {
-    			db.bulkDocs({docs : demoContacts }, function(err, response) {
-    				if (response) {
-    					console.log("Making demo contacts.");
-    					console.log(response);	
-    				} else {
-    					console.log("Error making demo contacts.");
-    					console.log(err);
-    				}
-    			});
-    		}
-    	});
+        // Pull content from the TDH database first
+        db.replicate.from(tdhdb);
+
+        // Check, if there are no contacts in the TDH, load up demo contacts.
+        db.info().then(function(info) {
+            // Initialize demo data
+            if (info.doc_count < 1) {
+                db.bulkDocs({docs : demoContacts }, function(err, response) {
+                    if (response) {
+                        console.log("Making demo contacts.");
+                        console.log(response);  
+                    } else {
+                        console.log("Error making demo contacts.");
+                        console.log(err);
+                    }
+                });
+            }
+        });
     });
