a3ab0e5eb37f6e3c4e68b924a2210795f7165d8f
Ivan R. Judson <irjudson@gmail.com>
2014-06-20 12:31:39 -0700

    End to end working build of the TDH UX

--- Production/Apps/ThaliClient/web/server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express'),
-    path = require('path');
-
-var app = express();
-app.use(express.bodyParser());
-app.use(express.static(path.join(__dirname, '.')));
-
-app.listen(3000);
-console.log('Listening on port 3000...');
