f11e2e178a6aa17c018b649045e7213b70cee5b7
unknown <yarong@microsoft.com>
2014-07-23 11:43:48 -0700

    Moved HttpKeyTypes (now with actual files!)
    HttpKeyTypes - Had to move this to its own class file so that Jackson
    could parse it, Jackson doesn't like non-static internal classes.

--- Production/Utilities/UniversalUtilities/src/main/java/com/msopentech/thali/CouchDBListener/HttpKeyTypes.java
+++ Production/Utilities/UniversalUtilities/src/main/java/com/msopentech/thali/CouchDBListener/HttpKeyTypes.java
@@ -1,8 +1,41 @@
+/*
+Copyright (c) Microsoft Open Technologies, Inc.
+All Rights Reserved
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+INCLUDING WITHOUT LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+MERCHANTABLITY OR NON-INFRINGEMENT.
+
+See the Apache 2 License for the specific language governing permissions and limitations under the License.
+*/
+
 package com.msopentech.thali.CouchDBListener;
 
-/**
- * Created by yarong on 7/23/2014.
- */
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.msopentech.thali.utilities.universal.HttpKeyURL;
+
 public class HttpKeyTypes {
-    public String localMachineIPHttpKeyURL = null;
+    protected String localMachineIPHttpKeyURL = null;
+
+    /**
+     * This constructor is just meant for use when parsing from JSON.
+     */
+    @JsonCreator
+    public HttpKeyTypes() {
+
+    }
+
+    public HttpKeyTypes(HttpKeyURL localMachineIpHttpKeyUrl) {
+        this.localMachineIPHttpKeyURL = localMachineIpHttpKeyUrl.toString();
+    }
+
+    public String getLocalMachineIPHttpKeyURL() {
+        return localMachineIPHttpKeyURL;
+    }
+
+    public void setLocalMachineIPHttpKeyURL(String localMachineIPHttpKeyURL) {
+        this.localMachineIPHttpKeyURL = localMachineIPHttpKeyURL;
+    }
 }
